35f4433bd0cf6d27062c0126ece8ac81fcb832e7
dmitry <Damon3453@yandex.ru>
2026-01-20 19:46:20 +0300

    add stubs

---
+++ stubs/arr.stub.php
@@ -0,0 +1,19 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Illuminate\Support;
+
+use ArrayAccess;
+
+/**
+ * @method static bool bool(ArrayAccess|array $array, string|int|null $key, mixed $default = null, bool $smart = true)
+ * @method static int int(ArrayAccess|array $array, string|int|null $key, mixed $default = null)
+ * @method static float toFloat(ArrayAccess|array $array, string|int|null $key, mixed $default = null)
+ * @method static string toString(ArrayAccess|array $array, string|int|null $key, mixed $default = null, bool $trim = false)
+ * @method static array toArray(ArrayAccess|array $array, string|int|null $key, array $default = [])
+ * @method static mixed toEnum(ArrayAccess|array $array, string|int|null $key, string $enumClass, mixed $default = null)
+ */
+final class Arr
+{
+}
---
+++ stubs/collection.stub.php
@@ -0,0 +1,16 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Illuminate\Support;
+
+use Illuminate\Pagination\LengthAwarePaginator;
+
+/**
+ * @method LengthAwarePaginator paginate(int $perPage, ?int $total = null, ?int $page = null, string $pageName = 'page')
+ * @method self filterNotNull()
+ * @method self filterNotBlank()
+ */
+final class Collection
+{
+}
